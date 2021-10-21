07f64cdb9d427361d56f31a1d9e53d50746e4415
bhanu <bhanubhaskar9705@gmail.com>
2021-10-21 14:26:41 +0530

    one

--- One.java
+++ One.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class one {
     public static void main(String[] args) {
-        String[] names = { "srinivas", "girish", "Tend", "bhanu" };
+        String[] names = { "srinivas", "girish", "Tendul", "bhanu" };
         List<String> ls = Arrays.asList(names);
         ArrayList al = new ArrayList(ls);
         System.out.println(al);
