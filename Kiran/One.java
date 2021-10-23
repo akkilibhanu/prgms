2f245c7635ba6ca829cf84bbd7fc57f601678e63
bhanu <bhanubhaskar9705@gmail.com>
2021-10-23 16:49:42 +0530

    One

---
+++ Kiran/One.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class One {
+    public static void main(String[] args) {
+        ArrayList a1 = new ArrayList();
+        a1.add("kiran");
+        a1.add("Bhanu");
+        a1.add("Tendul kuamr");
+        a1.add("Girish");
+        Collections.sort(a1);
+        Collections.sort(a1, Collections.reverseOrder());
+        System.out.println(a1.size());
+        System.out.println(a1.get(2));
+        System.out.println(a1);
+        System.out.println(a1);
+    }
+
+}
