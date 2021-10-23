d9655345a6bf8ec9f28240402a7a7ccc9b68196c
bhanu <bhanubhaskar9705@gmail.com>
2021-10-23 16:51:26 +0530

    test

--- Kiran/One.java
+++ Kiran/One.java
@@ -4,7 +4,7 @@ class One {
     public static void main(String[] args) {
         ArrayList a1 = new ArrayList();
         a1.add("kiran");
-        a1.add("Bhanu");
+        a1.add("Bhanutej");
         a1.add("Tendul kuamr");
         a1.add("Girish");
         Collections.sort(a1);
